8c95bbef5dc9cc70a228ec2b0eb3d35eada3d6a8
Tom Mullins <tmullins@andrew.cmu.edu>
2012-12-13 15:59:32 -0500

    Changed read util to flush input and wait for reads to complete

--- util/read.c
+++ util/read.c
@@ -14,12 +14,22 @@ void init_tty(int fd) {
   struct termios ios;
   bzero(&ios, sizeof(ios));
   ios.c_cflag = B9600 | CS8 | CREAD;
-  tcsetattr(fd, TCSANOW, &ios);
+  tcsetattr(fd, TCSAFLUSH, &ios);
 }
 
 int bswap(int x) {
   return ((x&0xff) << 24) | ((x&0xff00) << 8)
     | ((x>>8) & 0xff00) | ((x>>24) & 0xff);
+}
+
+void read_all(int fd, void *buf, int n) {
+  int nread;
+  while (n > 0) {
+    nread = read(fd, buf, n);
+    buf += nread;
+    n -= nread;
+    usleep(10000);
+  }
 }
 
 int main(int argc, char **argv) {
@@ -30,16 +40,17 @@ int main(int argc, char **argv) {
   init_tty(fd);
 
   while (1) {
+    tcflush(fd, TCIFLUSH);
     write(fd, read_cmd, sizeof(read_cmd));
-    read(fd, &status, 1);
+    read_all(fd, &status, 1);
     if (status == RFID_OK) {
-      read(fd, &serno, 4);
+      read_all(fd, &serno, 4);
       serno = bswap(serno);
       printf("%08x\n", serno);
     } else {
       printf("error %d\n", (int)(unsigned char)status);
     }
-    usleep(100000);
+    usleep(50000);
   }
 
   close(fd);
