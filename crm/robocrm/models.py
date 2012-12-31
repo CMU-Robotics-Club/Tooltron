cd771306951ff07fdd840959edd6db202fa022f6
Julian Binder <jabinder@andrew.cmu.edu>
2012-12-31 17:36:00 -0500

    Updated user model

--- crm/robocrm/models.py
+++ crm/robocrm/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
 # User Model
-class User(models.model):
-  andrewID = models.CharField(max_length=8)
-  first_name = models.CharField(max_length=30)
-  last_name = models.CharField(max_length=30)
+class RoboUser(AbstractBaseUser):
+  andrewID = models.CharField(max_length=8, unique=True, db_index=True)
+  
+
+
+
+  USERNAME_FIELD = 'andrewID'
   
