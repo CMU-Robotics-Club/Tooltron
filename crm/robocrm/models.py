a048cd77cc724d965b69d98a67fb3f3e308f4a59
Julian Binder <jabinder@andrew.cmu.edu>
2013-01-05 12:07:02 -0800

    fixed issue with models for form validation. Allowed fields to be blank that should be able to be blank.

--- crm/robocrm/models.py
+++ crm/robocrm/models.py
@@ -7,8 +7,8 @@ class Machine(models.Model):
   type = models.CharField(max_length=20)
   id = models.CharField(max_length=10, primary_key=True)
   maint = models.BooleanField(default=False)
-  dstart = models.DateTimeField()
-  dend = models.DateTimeField()
+  dstart = models.DateTimeField(blank=True)
+  dend = models.DateTimeField(blank=True)
   
   def __unicode__(self):
     return u'%s %s' % (self.type, self.id)
@@ -19,13 +19,13 @@ class RoboUser(models.Model):
   user = models.OneToOneField(User)
 
   # Roboclub RFID Card number
-  rfid = models.DecimalField(max_digits=10, decimal_places=0)
+  rfid = models.DecimalField(max_digits=10, decimal_places=0, blank=True)
   
   # Roboclub Shop Access Permissions
   machines = models.ManyToManyField(Machine)
 
   # Cell Phone
-  cell = models.DecimalField(max_digits=10, decimal_places=0)
+  cell = models.DecimalField(max_digits=10, decimal_places=0, blank=True)
 
   # Class Level
   FRESHMAN = 'FR'
@@ -51,8 +51,8 @@ class RoboUser(models.Model):
 
   # Primary and Secondary Major/Minors
   major = models.CharField(max_length=20)
-  sec_major_one = models.CharField(max_length=20)
-  sec_major_two = models.CharField(max_length=20)
+  sec_major_one = models.CharField(max_length=20, blank=True)
+  sec_major_two = models.CharField(max_length=20, blank=True)
 
   #Club Rank
   JUNIOR_MEM = 'JM'
@@ -129,10 +129,10 @@ class RoboResource(models.Model):
   type = models.CharField(max_length=30)
   id = models.CharField(max_length=20, primary_key=True)
   checked_out = models.BooleanField(default=False)
-  user = models.ForeignKey('RoboUser', related_name='u+')
-  time_out = models.DateTimeField()
-  time_due = models.DateTimeField()
-  officer = models.ForeignKey('RoboUser', related_name='o+')
+  user = models.ForeignKey('RoboUser', related_name='u+', blank=True)
+  time_out = models.DateTimeField(blank=True)
+  time_due = models.DateTimeField(blank=True)
+  officer = models.ForeignKey('RoboUser', related_name='o+', blank=True)
   def __unicode__(self):
     return u'%s %s %s' (self.type, self.id, self.checked_out)
 
