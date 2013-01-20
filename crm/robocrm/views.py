7cc67b057e54fe42718a8476563749814674c326
Julian Binder <jabinder@andrew.cmu.edu>
2013-01-20 16:37:44 -0500

    working on urls. views not working right

--- crm/robocrm/views.py
+++ crm/robocrm/views.py
@@ -1,10 +1,15 @@
 # Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+  return HttpResponse("Hello again, world!")
 
 def roboauth(request, rfid_tag, mach_number):
-  r = robocrm.objects.filter(rfid=rfid_tag)
-  auth_machines = r.machines.filter(id=mach_number)
-  if auth_machines.count() > 0 :
-    return HttpResponse("1")
-  else
-    return HttpResponse("0")
+  return HttpResponse("Hello, world!")
+  #r = robocrm.objects.filter(rfid=rfid_tag)
+  #auth_machines = r.machines.filter(id=mach_number)
+  #if auth_machines.count() > 0 :
+  #  return HttpResponse("1")
+  #else
+  #  return HttpResponse("0")
 
